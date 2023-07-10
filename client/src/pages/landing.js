f1cabb3be79c6c9bfa486384f8bd51ebae4bb6c6
steno1 <onuprinceley@gmail.com>
2023-07-10 18:24:13 +0100

    update code

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -15,9 +15,20 @@ function Landing(){
                 <div className="info">
                     <h1>Job<span>Finding</span>app</h1>
                     <p>
-         JobFindingApp is a MERN (MongoDB, Express, React, Node.js) 
-         stack application for job applications.It allows users to browse
-        and search for job listings, create and update their profiles.
+                    Find, Track, and Succeed with TrackJobApp
+
+Simplify your job search with TrackJobApp. Effortlessly track and 
+manage your applications, stay organized, and increase your chances
+ of success. Add job listings with details like company, position, 
+ and location. Monitor your application progress, from pending
+  interviews to declined offers. Create a professional profile to 
+  impress employers and showcase your skills. With an intuitive job
+   search feature, discover relevant opportunities based on your
+    preferences. Gain valuable insights with comprehensive statistics,
+     including interview status and user growth trends. Rest easy
+      knowing your data is secure with encrypted passwords and 
+      JWT authentication.Take control of your career and sign up for
+       TrackJobApp today!
                     </p>
      <Link to="/register" className="btn btn-hero">
        Login/Register
